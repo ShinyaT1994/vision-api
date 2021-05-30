7d24e46611b3e3afa2f807a3880fd65f421d783d
root <rm0063vpedc15@gmail.com>
2021-05-30 21:41:49 +0900

    Create upload section

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,20 @@
 const functions = require("firebase-functions");
 
+const admin = require('firebase-admin');
+admin.initializeApp();
+
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+
+
+
+// Trigger
+functions.storage.object().onFinalize(async (object) =>{
+    console.log("Trigger start!");
+});
