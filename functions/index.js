65007d960831f20b70adbf1a2e6096762145913f
root <rm0063vpedc15@gmail.com>
2021-06-03 22:10:18 +0900

    edit functions

--- functions/index.js
+++ functions/index.js
@@ -6,15 +6,17 @@ admin.initializeApp();
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
-});
+// exports.helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
 
 
 
 
 // Trigger
-const trigger = functions.storage.object().onFinalize(async (object) =>{
+const sample = functions.storage.object().onFinalize(async (object) =>{
     console.log("Trigger start!");
 });
+
+exports.sample();
